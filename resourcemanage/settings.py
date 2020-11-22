197f9c5b34b7aa33fdc472d575ca374d893fa75a
tiaoshe <990889214@qq.com>
2020-11-22 11:52:30 +0800

    2020 11 21 第3次部署
    Signed-off-by: tiaoshe <990889214@qq.com>

--- resourcemanage/settings.py
+++ resourcemanage/settings.py
@@ -165,7 +165,7 @@ TINYMCE_DEFAULT_CONFIG = {
 HAYSTACK_CONNECTIONS = {
     'default': {
         # 使用whoosh引擎
-        'ENGINE': 'haystack.backends.whoosh_cn_backend.WhooshEngine',
+        'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
         # 索引文件路径
         'PATH': os.path.join(BASE_DIR, 'whoosh_index'),
     }
