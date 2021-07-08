af541e39142b1202fab9c88357db8afcd2462069
tiaoshe <990889214@qq.com>
2021-07-09 01:13:59 +0800

    公司脚本

--- AppResourceManage/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from AppResourceManage.views import IndexView, DetailView, ResourceTypeView
-
-app_name = 'AppOrderManage'
-urlpatterns = [
-    path('index', IndexView.as_view(), name='index'),
-    path('detail/<int:resource_id>/', DetailView.as_view(), name='detail'),
-    path('type_list/<int:type_id>/', ResourceTypeView.as_view(), name='type_list')
-]
